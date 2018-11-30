a52c778ce741e891e01e075723b037b90d764705
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2018-11-30 11:51:28 +0100

    Updates

---
+++ precache-manifest.9247147ca5b4555e697370c89b925be9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "83edb383100e9928c103",
+    "url": "/moonsters/static/js/runtime~main.83edb383.js"
+  },
+  {
+    "revision": "3232c5b4497fba5c936e",
+    "url": "/moonsters/static/js/main.3232c5b4.chunk.js"
+  },
+  {
+    "revision": "360dcafafa8b45d83160",
+    "url": "/moonsters/static/js/1.360dcafa.chunk.js"
+  },
+  {
+    "revision": "3232c5b4497fba5c936e",
+    "url": "/moonsters/static/css/main.6c71ca48.chunk.css"
+  },
+  {
+    "revision": "360dcafafa8b45d83160",
+    "url": "/moonsters/static/css/1.848b3fb4.chunk.css"
+  },
+  {
+    "revision": "81d8dc18af45052b29769c1a82f1c8bd",
+    "url": "/moonsters/index.html"
+  }
+];
